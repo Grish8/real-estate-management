519a36acf1344ddbca9397e2b27a2f0745eea0bc
Grisham <doublegomosh00@gmail.com>
2024-03-11 04:36:49 +0300

    connect to database

--- api/index.js
+++ api/index.js
@@ -1,9 +1,18 @@
 import express from 'express'; //import express files which serves as the backend server.using(npm i express)
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
 
+mongoose.connect(process.env.MONGO).then (() => {
+    console.log("Connected to MongoDB!");
+
+}).catch((err) => {
+    console.log(err);
+});
 
 const app=express();
 
-app.listen(5173, () => {
-    console.log("Grish server is running on this port: 5173");
+app.listen(3000, () => {
+    console.log("Grish server is running on this port: 3000");
 }
 );
